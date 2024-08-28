1fa553699e40c2a7f63492e7bdf6dd51e70b18b2
MunJunYeong <msw711666@naver.com>
2024-08-28 15:25:56 +0900

    git config 설정 추가

--- develop.sh
+++ develop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Git 설정 (Local 설정)
+GIT_USERNAME=""
+GIT_EMAIL=""
+
 # Github repo URL 설정
 FRONTEND_REPO_URL="https://github.com/MunJunYeong/Ghost-Board-Frontend.git"
 BACKEND_REPO_URL="https://github.com/MunJunYeong/Ghost-Board-Backend.git"
@@ -13,21 +17,34 @@ BACKEND_DIR="backend"
 if [ -d "$FRONTEND_DIR" ]; then
   echo "Pulling latest changes..."
   cd "$FRONTEND_DIR"
+  
   git fetch 
   git pull origin master
   cd ..
 else
   echo "Cloning frontend repository..."
   git clone "$FRONTEND_REPO_URL" "$FRONTEND_DIR"
+
+  cd "$FRONTEND_DIR"
+  git config user.name "$GIT_USERNAME"
+  git config user.email "$GIT_EMAIL"
+  cd ..
 fi
 
 # BE - clone or pull
 if [ -d "$BACKEND_DIR" ]; then
   echo "Pulling latest changes..."
   cd "$BACKEND_DIR"
+  
+  git fetch
   git pull origin master
   cd ..
 else
   echo "Cloning backend repository..."
   git clone "$BACKEND_REPO_URL" "$BACKEND_DIR"
+
+  cd "$BACKEND_DIR"
+  git config user.name "$GIT_USERNAME"
+  git config user.email "$GIT_EMAIL"
+  cd ..
 fi
